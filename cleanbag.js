320d69ff0b2af8aa61f9eaedff3a85aad4e5139f
Felixvv <73643402+GlitchyBoy@users.noreply.github.com>
2024-06-06 20:15:58 +0200

    Timer update
    Added a timer
    Added a co2 counter
    Added the reviews
    Fixed some minor bugs.

--- cleanbag.js
+++ cleanbag.js
@@ -10,7 +10,15 @@ if (is_mobile === true) {
 }
 
 
-let timeS = 0;
+let timeS = {
+  second: 0,
+  seconds: 0,
+  minute: 0,
+  minutes: 0
+};
+
+let co2 = 416;
+
 timeLoop();
 
 function timeLoop () {
@@ -18,8 +26,29 @@ function timeLoop () {
 }
 
 function time () {
-  timeS = timeS + 1;
-  console.log(timeS);
+  timeS.second = timeS.second + 1;
+  console.log(timeS)
+
+  if (timeS.second === 10) {
+    timeS.second = 0;
+    timeS.seconds = timeS.seconds + 1;
+  
+  } else if (timeS.seconds === 6) {
+    timeS.seconds = 0;
+    timeS.minute = timeS.minute +1;
+  } else if (timeS.minute === 10) {
+    timeS.minute = 0;
+    timeS.minutes = timeS.minutes +1;
+  };
+
+  document.getElementById("second").innerHTML = `${timeS.second}`;
+  document.getElementById("seconds").innerHTML = `${timeS.seconds}`;
+  document.getElementById("minute").innerHTML = `${timeS.minute}:`;
+  document.getElementById("minutes").innerHTML = `${timeS.minutes}`;
+
+  co2 = co2 + 416;
+  document.getElementById("co2").innerHTML = `${co2 / 1000}`;
+
   timeLoop();
 
  /* if (timeS === 60) {
